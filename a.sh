93c3adcf7dd0cfea02ea4720838f9e9133d4a29e
Tran Duy Dat <dattd@apecsoft.asia>
2020-12-18 10:11:27 +0700

    run example 1

---
+++ a.sh
@@ -0,0 +1,11 @@
+cd "./A"
+# cd "./RN_Core"
+# mv "src_1" "../"
+# cd ".."
+
+mv src_1/App.js ./
+mv src_1/init.sh ./
+mv src_1/build_apk.sh ./
+mv src_1/jsconfig.json ./
+mv src_1/clean_android.sh ./
+mv src_1/babel.config.js ./
